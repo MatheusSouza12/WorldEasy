f53e92df1582acd1913622eef2eda70e731cc818
Matheus Souza <matheus007junior@hotmail.com>
2016-10-25 23:00:05 -0200

    Up

--- app/src/main/java/com/example/matheus/worldeasy/BancoController.java
+++ app/src/main/java/com/example/matheus/worldeasy/BancoController.java
@@ -1,34 +1,34 @@
-//package com.example.matheus.worldeasy;
-//
-//
-//import android.content.ContentValues;
-//import android.content.Context;
-//import android.database.sqlite.SQLiteDatabase;
-//
-//import static com.example.matheus.worldeasy.CriaBanco.COLUNA_NOME;
-//
-//public class BancoController {
-//    private SQLiteDatabase db;
-//    private CriaBanco banco;
-//    private final Context context;
-//
-//    public BancoController(Context context, String name, SQLiteDatabase.CursorFactory factory, int version)){
-//        banco= new CriaBanco(context,, null, VERSAO);
-//        this.context = context;
-//    }
-//    public void insereDado(String nome, String senha, String cpf){
-//        ContentValues valores;
-//        long resultado;
-//
-//        db=banco.getWritableDatabase();
-//        valores = new ContentValues();
-//        valores.put(COLUNA_NOME, nome);
-//        valores.put(COLUNA_EMAIL, email);
-//
-//
-//        resultado = db.insert(CriaBanco.DB_TABELA, null, valores);
-//        db.close();
-//
-//
-//    }
-//}
+package com.example.matheus.worldeasy;
+
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import static com.example.matheus.worldeasy.CriaBanco.COLUNA_NOME;
+
+public class BancoController {
+    private SQLiteDatabase db;
+    private CriaBanco banco;
+    private final Context context;
+
+    public BancoController(Context context, String name, SQLiteDatabase.CursorFactory factory, int version)){
+        banco= new CriaBanco(context,, null, VERSAO);
+        this.context = context;
+    }
+    public void insereDado(String nome, String senha, String cpf){
+        ContentValues valores;
+        long resultado;
+
+        db=banco.getWritableDatabase();
+        valores = new ContentValues();
+        valores.put(COLUNA_NOME, nome);
+        valores.put(COLUNA_EMAIL, email);
+
+
+        resultado = db.insert(CriaBanco.DB_TABELA, null, valores);
+        db.close();
+
+
+    }
+}
